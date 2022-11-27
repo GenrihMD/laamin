62e1aae8a2737de5a854d46d1a341753b20bd222
alexanderl <alexanderl@kmslh.com>
2022-11-28 01:00:43 +0300

    Add redraw laamin

--- app/components/TheLaamin.js
+++ app/components/TheLaamin.js
@@ -1,6 +1,4 @@
-import { t } from "../lib/html.js"
-
-const template = t`
+const template = `
     <main
         style="
             display: flex;
@@ -16,30 +14,37 @@ const template = t`
 
 class TheLaamin extends HTMLElement {
 
-    constructor(props) {
+    constructor() {
         super();
 
         const shadowRoot = this.attachShadow({ mode: "open" });
-        shadowRoot.appendChild(template.cloneNode(true));
-
-        this.drawLaamin()
+        shadowRoot.innerHTML = template
     }
 
-    drawLaamin() {
+    connectedCallback() {
         const LAAMIN = 'laamin'
+        this.draw(LAAMIN)
+        this.addEventListener('draw', () => {
+            this.draw(LAAMIN)
+        })
+    }
+
+    draw(text) {
         const canv = this.shadowRoot.getElementById('laamin-canvas')
         const ctx = canv.getContext('2d')
+
+        const getRandX = () => canv.width * Math.random();
+        const getRandY = () => canv.height * Math.random();
+        const getRandSize = () => 30 + Math.floor(150 * Math.random())
 
         ctx.width = 2000
         ctx.height = 2000
 
-        const textMetrics =  ctx.measureText(LAAMIN);
-
-        console.log(textMetrics.width, textMetrics)
+        const textMetrics =  ctx.measureText(text);
 
         ctx.textAlign = "center";
-        ctx.font = '200px sans-serif';
-        ctx.fillText(LAAMIN, canv.width/2, canv.height/2);
+        ctx.font = getRandSize() + 'px sans-serif';
+        ctx.fillText(text, getRandX(), getRandY());
     }
 
 }
--- app/main.js
+++ app/main.js
@@ -1,3 +1,7 @@
 import './components/TopMenu/TopMenu.js'
 import './components/TopMenu/MenuItem.js'
 import './components/TheLaamin.js'
+
+document.addEventListener('laamin:re', () => {
+    document.getElementsByTagName('the-laamin').item(0).dispatchEvent(new Event('draw'))
+})
