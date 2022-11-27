625fa113b711b08bd45809e093fbfe5ea2a0a876
alexanderl <alexanderl@kmslh.com>
2022-11-28 01:00:24 +0300

    Add submenu

--- app/components/TopMenu/MenuItem.js
+++ app/components/TopMenu/MenuItem.js
@@ -1,6 +1,4 @@
-import { t } from "../../lib/html.js"
-
-const template = t`
+const template = `
     <style>
         * {
             cursor: pointer;
@@ -10,11 +8,22 @@ const template = t`
             -ms-user-select: none;
             user-select: none;
         }
+        #sub-menu {
+            display: none;
+            position: absolute;
+            background: black;
+            color: white;
+            padding: 5px 2px;
+            top: 100%;
+            left: 0;
+        }
     </style>
     
-    <span>
-        <slot></slot>
-    </span>
+    <slot></slot>
+    
+    <div id="sub-menu">
+        <slot name="submenu"></slot>
+    </div>
 `
 
 class MenuItem extends HTMLElement {
@@ -22,14 +31,41 @@ class MenuItem extends HTMLElement {
     constructor(props) {
         super();
 
-        const shadowRoot = this.attachShadow({ mode: "open" });
-        shadowRoot.appendChild(template.cloneNode(true));
+        const shadowRoot = this.attachShadow({ mode: "open" })
+        shadowRoot.innerHTML = template
+        this.style.position = 'relative'
     }
 
     connectedCallback() {
-        const href = this.getAttribute('to')
-        if (href) {
-            this.addEventListener('click', () => location.href = href )
+        const type = this.getAttribute('type')
+
+        switch (type) {
+            case 'link':
+                const href = this.getAttribute('href')
+                if (href) {
+                    this.addEventListener('click', () => location.href = href )
+                }
+                break
+
+            case 'menu':
+                this.addEventListener('mouseover', () => {
+                    this.shadowRoot.getElementById('sub-menu').style.display = 'block'
+                })
+
+                this.addEventListener('mouseout', () => {
+                    this.shadowRoot.getElementById('sub-menu').style.display = 'none'
+                })
+                break
+
+            case 'action':
+                const action = this.getAttribute('action')
+                if (action) {
+                    this.addEventListener('click', () => {
+                        const event = new Event(action, {"bubbles":true, "cancelable":false})
+                        document.dispatchEvent(event)
+                    })
+                }
+                break
         }
     }
 
