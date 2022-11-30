a7a6e49c0ab9ebe3384f61e9e72bc624afe4de77
alexanderl <alexanderl@kmslh.com>
2022-11-30 19:36:27 +0300

    Add event handlers

--- app/main.js
+++ app/main.js
@@ -2,6 +2,12 @@ import './components/TopMenu/TopMenu.js'
 import './components/TopMenu/MenuItem.js'
 import './components/TheLaamin.js'
 
-document.addEventListener('laamin:re', () => {
-    document.getElementsByTagName('the-laamin').item(0).dispatchEvent(new Event('draw'))
+const theLaamin = document.getElementsByTagName('the-laamin').item(0);
+
+document.addEventListener('laamin:draw', () => {
+    theLaamin.dispatchEvent(new Event('draw'))
+})
+
+document.addEventListener('laamin:clear', () => {
+    theLaamin.dispatchEvent(new Event('clear'))
 })
